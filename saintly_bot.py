466eaea1e79af4f604e889641351cd8ba35e0fcb
realendie <enderprooffical@gmail.com>
2025-08-23 18:43:53 -0400

    Updated saintly_bot.py with code from test.py

--- saintly_bot.py
+++ saintly_bot.py
@@ -1,38 +1,40 @@
-print("Starting Saintly Bot...")
-
-print("Loading libraries...")
 import discord
+from discord.ext import commands
 from discord import app_commands
 import os
-import time
 
-# Accesing bot token from environment variable
-print("Fetching bot token...")
 token = os.getenv("SAINTLY_BOT_TOKEN")
 
-# Bot discord intents
-print("Setting Intents")
+
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f"We have logged in as {self.user}")
+
+        try:
+            guild = discord.Object(id=1366991874019168256)
+            synced = await self.tree.sync(guild=guild)
+            print(f"Synced {len(synced)} commands to the guild {guild}.")
+        except Exception as e:
+            print(f"Failed to sync commands: {e}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("!hello"):
+            await message.channel.send(f"Hello {message.author.mention}!")
+
+
 intents = discord.Intents.default()
-intents.messages = True
-intents.guilds = True
-intents.members = True
-client = discord.Client(intents=intents)
+intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-# Command tree
-print("Setting up command tree...")
-tree = app_commands.CommandTree(client)
+GUILD_ID = discord.Object(id=1366991874019168256)  # Saintly City Discord Server ID
 
 
-print("Connecting to Discord...")
-
-
-@client.event
-async def on_ready():
-    currenttime = time.localtime()
-    format_time = time.strftime("%H:%M:%S", currenttime)
-
-    print(f"Bot logged in as {client.user}")
-    print(f"Bot online as of {format_time}!")
+@client.tree.command(name="hello", description="Say hello!", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello there! test")
 
 
 client.run(token)
