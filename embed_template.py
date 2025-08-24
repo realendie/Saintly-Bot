ef8250b3c1fb7dfe43f9fff8acf934078d4aeef5
realendie <enderprooffical@gmail.com>
2025-08-24 16:18:27 -0400

    Made sample embed template for future use

---
+++ embed_template.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+embed = discord.Embed(
+    color=8496575,
+    title="Title",
+    description="Description",
+)
+embed.set_thumbnail(
+    url="https://cdn.discordapp.com/attachments/1385571219276955678/1408971677643182080/SCRP-Logo.png?ex=68abaea1&is=68aa5d21&hm=2526ac37367c6e93393728b38cf6cc7dd782c0f0b34a23d61807f1c94eecd5d8&"
+)
