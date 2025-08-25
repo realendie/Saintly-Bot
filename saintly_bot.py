c6ae03cd5d52f737bb4c6a415fa991555faf2726
realendie <enderprooffical@gmail.com>
2025-08-25 18:36:34 -0400

    Added ban logging and functionality

--- saintly_bot.py
+++ saintly_bot.py
@@ -70,12 +70,15 @@ async def f8(interaction: discord.Interaction):
         "To join the server via the f8 menu press f8 then paste this command:\n```cfx.re/join/o7edmx```"
     )
 
+
 # Current Time Command
-@client.tree.command(name="time", description="Gives the user the current time and date in UTC", guild=GUILD_ID)
+@client.tree.command(
+    name="time",
+    description="Gives the user the current time and date in UTC",
+    guild=GUILD_ID,
+)
 async def timeCommand(interaction: discord.Interaction):
-    await interaction.response.send_message(
-        f"It is currently **{formated_time}**"
-    )
+    await interaction.response.send_message(f"It is currently **{formated_time}**")
 
 
 # Moderation Tools
@@ -121,11 +124,15 @@ async def kick(
         mod_logs = interaction.guild.get_channel(MOD_LOGS_ID)
         if mod_logs:
             embed = discord.Embed(
-            color=8496575,
-            title="Kick Log",
-        )
+                color=8496575,
+                title="Kick Log",
+            )
         embed.add_field(name="User", value=f"{user} (`{user.id}`)", inline=False)
-        embed.add_field(name="Kicked by", value=f"{interaction.user} (`{interaction.user.id}`)", inline=False)
+        embed.add_field(
+            name="Kicked by",
+            value=f"{interaction.user} (`{interaction.user.id}`)",
+            inline=False,
+        )
         embed.add_field(name="Reason", value=reason, inline=False)
         embed.set_thumbnail(url=user.display_avatar.url)
         embed.set_footer(text=f"Kicked on {formated_time} UTC")
@@ -140,6 +147,72 @@ async def kick(
         else:
             await interaction.response.send_message(
                 f"Failed to kick {user}. Error: {e}", ephemeral=True
+            )
+
+
+# Ban Command
+@client.tree.command(
+    name="ban",
+    description="Bans a user from the server and logs it in #mod-logs",
+    guild=GUILD_ID,
+)
+@app_commands.describe(user="User to ban", reason="Reason for baning")
+async def kick(
+    interaction: discord.Interaction,
+    user: discord.Member,
+    reason: str = "No reason provided",
+):
+    # Checking for ban permissions
+    if not interaction.user.guild_permissions.ban_members:
+        return await interaction.response.send_message(
+            "You do not have permission to run this command.", ephemeral=True
+        )
+
+    # Stops user from banning higher or equal role members
+    if user.top_role >= interaction.user.top_role:
+        return await interaction.response.send_message(
+            "You cannot ban this user.", ephemeral=True
+        )
+    # Stops user from banning themselves
+    if user == interaction.user:
+        return await interaction.response.send_message(
+            "You cannot ban yourself.", ephemeral=True
+        )
+
+    # Banning Functionality
+    try:
+        await user.ban(reason=reason)
+        await interaction.response.send_message(
+            f"**{user}** has been banned from the server."
+        )
+
+        # Ban Logging
+        mod_logs = interaction.guild.get_channel(MOD_LOGS_ID)
+        if mod_logs:
+            embed = discord.Embed(
+                color=8496575,
+                title="Ban Log",
+            )
+        embed.add_field(name="User", value=f"{user} (`{user.id}`)", inline=False)
+        embed.add_field(
+            name="Banned by",
+            value=f"{interaction.user} (`{interaction.user.id}`)",
+            inline=False,
+        )
+        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.set_thumbnail(url=user.display_avatar.url)
+        embed.set_footer(text=f"Banned on {formated_time} UTC")
+
+        await mod_logs.send(embed=embed)
+    except Exception as e:
+        # If we already had responded we must send message as follow up
+        if interaction.response.is_done():
+            await interaction.followup.send(
+                f"Failed to Ban {user}. Error: {e}", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                f"Failed to Ban {user}. Error: {e}", ephemeral=True
             )
 
 
