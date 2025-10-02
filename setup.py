f4759bf9b7daceedec96e3b6a3c2f8ff4376e15a
Wyatt Johnson <enderprooffical@gmail.com>
2025-10-02 16:14:45 -0400

    Removed unused environment variables

--- setup.py
+++ setup.py
@@ -8,20 +8,4 @@ print(f"{ascii_banner}\b")
 os.environ["SAINTLY_BOT_TOKEN"] = input("Enter your bot token: ")
 print("Saintly Bot Token:", os.environ["SAINTLY_BOT_TOKEN"])
 
-# Guild ID
-os.environ["SERVER_ID"] = input("Enter your guild ID: ")
-print("Guild ID:", os.environ["SERVER_ID"])
-
-# Welcome Channel ID
-os.environ["WELCOME_CHANNEL_ID"] = input("Enter your welcome channel ID: ")
-print("Welcome Channel ID:", os.environ["WELCOME_CHANNEL_ID"])
-
-os.environ["PENDING_ROLE_ID"] = input("Enter your pending role ID: ")
-print("Pending Role ID:", os.environ["PENDING_ROLE_ID"])
-
-
-# Mod Log Channel ID
-os.environ["MOD_LOG_CHANNEL_ID"] = input("Enter your mod log channel ID: ")
-print("Mod Log Channel ID:", os.environ["MOD_LOG_CHANNEL_ID"])
-
-print('Setup complete!')
+print("Setup complete!")
